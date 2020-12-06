d2f9523f0040e66d0bd55d5c95be8d66da6c87f1
Nicola Orlando <nicolaorlando24@gmail.com>
2020-12-06 19:59:50 +0100

    Adding an interface (behemot commit, I know, don't care)

---
+++ client/init.lua
@@ -0,0 +1,74 @@
+require "love.event"
+require "love.graphics"
+require "love.image"
+require "love.keyboard"
+require "love.math"
+require "love.mouse"
+require "love.font"
+
+require "love.timer"
+require "love.touch"
+require "love.window"
+
+
+love.window.setMode(1280, 720)
+
+states = {}
+states.game = require "game"
+states.menu = require "menu"
+
+state = states.menu
+
+love.update = function(dt)
+  if state.update then
+    state:update(dt)
+  end
+end
+
+love.draw = function()
+  if state.draw then
+    state:draw()
+  end
+end
+
+love.mousepressed = function(x, y, b)
+  if state.mousepressed then
+    state:mousepressed(x, y, b)
+  end
+end
+
+love.keypressed = function(k, kk)
+  if state.keypressed then
+    state:keypressed(k, kk)
+  end
+end
+
+love.textinput = function(char)
+  if state.textinput then
+    state:textinput(char)
+  end
+end
+
+love.mousemoved = function(x, y, dx, dy)
+  if state.mousemoved then
+    state:mousemoved(x, y, dx, dy)
+  end
+end
+
+love.mousereleased = function(x, y, b)
+  if state.mousereleased then
+    state:mousereleased(x, y, b)
+  end
+end
+
+love.wheelmoved = function(wx, wy)
+  if state.wheelmoved then
+    state:wheelmoved(wx, wy)
+  end
+end
+
+love.quit = function()
+  if state.quit then
+    state:quit()
+  end
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+--[[
 love.load = function(args)
 	if args[1] == "--server" then
 		require "server"
@@ -8,4 +9,62 @@ love.load = function(args)
 		print("--server or --client wasn't specified - defaulting to --client nixo.la:42068")
 		require "client"
 	end
+end
+--]]
+config = {}
+
+enet = require "enet"
+
+string.split = function(str, sep)
+  assert(#sep == 1, "FUCK OFF")
+  local t = {}
+  for piece in str:gmatch("([^" .. sep .. "]+)") do
+    t[#t+1] = piece
+  end
+  return t
+end
+
+string.valid = function(last, guess)
+	local max = math.min(#last, #guess)
+	for i = 1, max do
+		if last:sub(-i, -1) == guess:sub(1, i) then
+			return true
+		end
+	end
+	return false
+end
+
+local load = function(args)
+  if love then
+    table.remove(arg, 1)
+  end
+  for i, v in ipairs(arg) do
+    if v:match("^%-%-") then --option
+      config[v:match("^%-%-(.-)$")] = true
+    else --par
+      local o = arg[i-1]:match("^%-%-(.-)$")
+      if o then
+        config[o] = v
+      end
+    end
+  end
+
+  if config.server then
+    if love then
+      love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";server/?.lua")
+    end
+    package.path = package.path .. ";./server/?.lua"
+    require "init"
+  else
+    assert(love, "You can't run a client without LÖVE.")
+    love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";client/?.lua")
+    package.path = package.path .. ";./client/?.lua"
+    require "client.init"
+  end
+end
+
+if love then
+  love.load = load
+else
+  load({...})
 end
